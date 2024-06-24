2ae879b427b4bfad67656ce27856c3af8fc32c3c
Kristina <kkristina.kolesnyk@gmail.com>
2024-06-24 19:20:03 +0300

    commit

--- app/src/main/java/com/kristina/pawsomeduo/MainActivity.java
+++ app/src/main/java/com/kristina/pawsomeduo/MainActivity.java
@@ -1,25 +1,104 @@
 package com.kristina.pawsomeduo;
 
-import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.appcompat.widget.AppCompatTextView;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import com.google.android.material.textview.MaterialTextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    private TextView home_TXT_title,home_TXT_intro;
+    private Spinner home_SP_count;
+    private AppCompatTextView home_TXT_option1,home_TXT_option2;
+    private MaterialTextView home_TXT_stats;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        //chang1 1
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        findViews();
+        createSpinner();
+        createListeners();
+
+    }
+
+    private void createListeners() {
+        home_SP_count.setOnItemSelectedListener(this);
+        home_TXT_option1.setOnClickListener(v-> option1Selected());
+        home_TXT_option2.setOnClickListener(v-> option2Selected());
+        home_TXT_stats.setOnClickListener(v->statsClicked());
+
+    }
+
+    private void option1Selected() {
+        home_TXT_option1.setBackgroundResource(R.color.selected);
+        home_TXT_option2.setBackgroundResource(R.color.spinner_2);
+        MSP.getInstance().putString("PURPOSE", home_TXT_option1.getText().toString());
+    }
+    private void option2Selected() {
+        home_TXT_option2.setBackgroundResource(R.color.selected);
+        home_TXT_option1.setBackgroundResource(R.color.spinner_1);
+        MSP.getInstance().putString("PURPOSE", home_TXT_option2.getText().toString());
+    }
+
+    private void statsClicked() {
+        startActivity(new Intent(this, AppManager.statsClass));
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+        String parameter = adapterView.getItemAtPosition(position).toString();
+        int i = parameter.indexOf(' ');
+        String word;
+        if (i >= 0) {
+            word = parameter.substring(0, i);
+            //MSP.getInstance().putString("COUNT", word);
+        } else {
+            word = parameter.toString();
+            // handle case where parameter does not contain a space character
+        }
+        //String word = parameter.substring(0, i);
+        MSP.getInstance().putString("COUNT", word);
+    }
+
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private void createSpinner() {
+
+        List<String> list = new ArrayList<>();
+        for (int i=1; i<=10; i++){
+            list.add(""+ i);
+        }
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
+                R.layout.activity_spinner, list);
+        dataAdapter.setDropDownViewResource(R.layout.activity_spinner);
+        home_SP_count.setAdapter(dataAdapter);
+        home_SP_count.setSelection(0);
+    }
+
+    private void findViews() {
+        home_TXT_title = findViewById(R.id.home_TXT_title);
+        home_TXT_intro = findViewById(R.id.home_TXT_intro);
+        home_SP_count = findViewById(R.id.home_SP_count);
+        home_TXT_option1 = findViewById(R.id.home_TXT_option1);
+        home_TXT_option2 = findViewById(R.id.home_TXT_option2);
+        home_TXT_stats = findViewById(R.id.home_TXT_stats);
+
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "PawsomeDuo"
 include(":app")
- 
+include(":cats")
